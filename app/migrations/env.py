e8d6e3adb83d5a8eb65d72f5f568cbf3b1376e22
Iago Estrela <IagoFilipe.EstrelaBarros@windriver.com>
2022-06-21 13:57:11 -0300

    add order table

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -9,6 +9,8 @@ from sqlalchemy import engine_from_config, pool
 from app.db.base import BaseMeta
 from app.models.items import Category, Item
 from app.models.users import User
+from app.models.orders import Payment
+from app.models.orders import Order
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -71,7 +73,9 @@ def run_migrations_online():
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata, include_schemas=True
+            connection=connection,
+            target_metadata=target_metadata,
+            include_schemas=True,
         )
 
         with context.begin_transaction():
--- app/models/orders.py
+++ app/models/orders.py
@@ -1,16 +1,44 @@
-from typing import List, Optional
+import uuid
+from enum import Enum
+from typing import List
 
 import ormar
 
 from app.core.models import AppBaseModel
 from app.db.base import BaseMeta
+from app.models.items import Item
+from app.models.users import User
+
+
+class PaymentMethod(Enum):
+    CREDIT_CARD = "CREDIT_CARD"
+    DEBIT_CARD = "DEBIT_CARD"
+    PIX = "PIX"
+
+
+class OrderState(Enum):
+    PENDING = "PENDING"
+    APPROVED = "APPROVED"
+
+
+class Payment(ormar.Model, AppBaseModel):
+    class Meta(BaseMeta):
+        tablename = "payments"
+
+    method: str = ormar.String(max_length=100, choices=list(PaymentMethod))
+
+
+class OrderItem(ormar.Model, AppBaseModel):
+    class Meta(BaseMeta):
+        tablename = "orders_items"
 
 
 class Order(ormar.Model, AppBaseModel):
     class Meta(BaseMeta):
         tablename = "orders"
 
-    # name: str = ormar.String(max_length=128, unique=True, nullable=False)
-    # state =
-    # amount: int = ormar.Integer()
-    # categories: Optional[List[Item]] = ormar.ManyToMany(Item, through=ItemCategory)
+    state: str = ormar.String(max_length=100, choices=list(OrderState))
+    payment: int = ormar.ForeignKey(Payment, nullable=True)
+    user_id: uuid.UUID = ormar.ForeignKey(User)
+    total: int = ormar.Integer()
+    items: List[Item] = ormar.ManyToMany(Item, through=OrderItem)
